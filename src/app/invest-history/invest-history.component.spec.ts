11218e9736a5488841abc357d95a4362bd372dc0
Lukasz Kipka <lukasz.kipka@pb.com>
2023-04-04 14:34:32 +0200

    Add Invest History component

---
+++ src/app/invest-history/invest-history.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { InvestHistoryComponent } from './invest-history.component';
+
+describe('InvestHistoryComponent', () => {
+  let component: InvestHistoryComponent;
+  let fixture: ComponentFixture<InvestHistoryComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ InvestHistoryComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(InvestHistoryComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/invest-history/invest-history.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-invest-history',
+  templateUrl: './invest-history.component.html',
+  styleUrls: ['./invest-history.component.scss']
+})
+export class InvestHistoryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
